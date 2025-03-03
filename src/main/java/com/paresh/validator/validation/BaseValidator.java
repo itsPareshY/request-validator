a7f906fe854b9293cdbae9b890c66d565ffc9769
itsPareshY <itspareshyadav@gmail.com>
2025-03-04 00:10:40 +0530

    default validation

--- src/main/java/com/paresh/validator/validation/BaseValidator.java
+++ src/main/java/com/paresh/validator/validation/BaseValidator.java
@@ -2,6 +2,7 @@ package com.paresh.validator.validation;
 
 import java.lang.reflect.Field;
 import javax.validation.constraints.Size;
+import com.paresh.validator.annotations.FreeText;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,15 +23,30 @@ public class BaseValidator implements Validator {
                 } else if (value instanceof String) {
                     String strValue = (String) value;
                     Size sizeAnnotation = field.getAnnotation(Size.class);
+                    FreeText freeTextAnnotation = field.getAnnotation(FreeText.class);
+
+                    if (freeTextAnnotation != null) {
+                        // Skip validation for FreeText fields
+                        continue;
+                    }
+
                     if (sizeAnnotation != null) {
-                        int maxLength = sizeAnnotation.max();
-                        if (strValue.length() > maxLength) {
-                            errors.put(field.getName(), "Length must not exceed " + maxLength + " characters");
+                        // Validate based on @Size annotation
+                        int maxSize = sizeAnnotation.max();
+                        if (strValue.length() > maxSize) {
+                            throw new ValidationException("Field " + field.getName() + " exceeds max length of " + maxSize);
+                        }
+                    } else {
+                        // Default validation for fields without @Size or @FreeText
+                        if (strValue.length() > 255) {
+                            throw new ValidationException("Field " + field.getName() + " exceeds default max length of 255");
                         }
                     }
                 }
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
+            } catch (ValidationException e) {
+                errors.put(field.getName(), e.getMessage());
             }
         }
     
@@ -44,5 +60,11 @@ public class BaseValidator implements Validator {
     @Override
     public void setNext(Validator next) {
         this.next = next;
+    }
+}
+
+class ValidationException extends Exception {
+    public ValidationException(String message) {
+        super(message);
     }
 }
